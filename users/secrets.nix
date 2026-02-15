355f2301142cb9d9e9ff74dc3b223bcea9d1db68
Jaakko Paju <36770267+JPaju@users.noreply.github.com>
2026-02-15 14:56:58 +0200

    secrets: add google generative AI API key

--- users/secrets.nix
+++ users/secrets.nix
@@ -2,5 +2,6 @@
 {
   secrets.anthropic_api_key = systemSops.secrets.anthropic_api_key.path;
   secrets.openai_api_key = systemSops.secrets.openai_api_key.path;
+  secrets.google_generative_ai_api_key = systemSops.secrets.google_generative_ai_api_key.path;
   secrets.context7_api_key = systemSops.secrets.context7_api_key.path;
 }
